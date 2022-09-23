0eab2179747a1467ef0a09be2f9778156e90ad12
tema0 <tema0090@gmail.com>
2022-09-24 01:08:54 +0300

    ver_0.004

--- proj3/news/models.py
+++ proj3/news/models.py
@@ -2,13 +2,45 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=150,
+                             verbose_name='Name of news')
+
+    content = models.TextField(blank=True,
+                               verbose_name='content fo article')
+
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      verbose_name='Date of create')
+
+    updated_at = models.DateTimeField(auto_now=True,
+                                      verbose_name='Date of update')
+
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/',
+                              verbose_name='Photo of article',
+                              blank=True)
+
+    is_published = models.BooleanField(default=True,
+                                       verbose_name='Published Yes/No')
+
+    category = models.ForeignKey('Category',
+                                 on_delete=models.PROTECT,
+                                 null=True,
+                                 )
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150,
+                             db_index=True,
+                             verbose_name='Name of category',
+                             )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
